54f982d8299c24a3010f41b99e7b85af65524513
sergiu_derecichei <d93sergiu@gmail.com>
2023-10-19 00:05:08 +0300

    Add simple flipper from defined Array

--- color_flipper/script.js
+++ color_flipper/script.js
@@ -1,11 +1,16 @@
-const colors=["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
-  const randomNumber = 2;
+  const randomNumber = getRandomNumber();
   document.body.style.backgroundColor = colors[randomNumber];
-  color.textContent =colors[randomNumber];
-})
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
 
