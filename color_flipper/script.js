5eabff4fbd2cdbb0c02d922ee7b9c82831e0d7d5
sergiu_derecichei <d93sergiu@gmail.com>
2023-10-19 23:56:42 +0300

    Add HEX version

--- color_flipper/script.js
+++ color_flipper/script.js
@@ -5,12 +5,27 @@ const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
-  const randomNumber = getRandomNumber();
-  document.body.style.backgroundColor = colors[randomNumber];
-  color.textContent = colors[randomNumber];
+  // const randomNumber = getRandomNumber();
+  // document.body.style.backgroundColor = colors[randomNumber];
+  // color.textContent = colors[randomNumber];
+
+  const hex = getHex();
+  document.body.style.backgroundColor = hex;
+  color.textContent = hex;
 });
 
 function getRandomNumber() {
   return Math.floor(Math.random() * colors.length);
 }
 
+// #FAB234
+
+function getHex() {
+  let color = "#";
+  for (i = 0; i < 6; i++) {
+    let random = Math.floor(Math.random() * hex.length);
+    let value = hex[random];
+    color += value;
+  }
+  return color;
+}
