377c88ff11f6f2c6b0a4f6a273436a2776ebd80c
sergiu_derecichei <d93sergiu@gmail.com>
2023-10-23 01:42:58 +0300

    Update Counter logic for all buttons

--- counter/script.js
+++ counter/script.js
@@ -1,42 +1,22 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-
 let count = 0;
-const btn = document.getElementById("");
+
 let number = document.querySelector(".number");
+let btns = document.querySelectorAll(".btn");
 
 const inc = document.querySelector(".increase");
 const dec = document.querySelector(".decrease");
 const reset = document.querySelector(".reset");
 
-inc.addEventListener("click", function () { 
-  console.log(number);
-  number.textContent = count++;
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const btnType = e.currentTarget.classList;
+    if (btnType.contains("increase")) {
+      count++;
+    } else if (btnType.contains("decrease")) {
+      count--;
+    } else {
+      count = 0;
+    }
+    number.textContent = count;
+  });
 });
-
-
-// btn.addEventListener("click", function () {
-//   // const randomNumber = getRandomNumber();
-//   // document.body.style.backgroundColor = colors[randomNumber];
-//   // color.textContent = colors[randomNumber];
-
-//   const hex = getHex();
-//   document.body.style.backgroundColor = hex;
-//   color.textContent = hex;
-// });
-
-// function getRandomNumber() {
-//   return Math.floor(Math.random() * colors.length);
-// }
-
-// // #FAB234
-
-// function getHex() {
-//   let color = "#";
-//   for (i = 0; i < 6; i++) {
-//     let random = Math.floor(Math.random() * hex.length);
-//     let value = hex[random];
-//     color += value;
-//   }
-//   return color;
-// }
