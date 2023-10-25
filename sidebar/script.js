695797c531a24da2c76cd88057928ac7daa0d7b6
sergiu_derecichei <d93sergiu@gmail.com>
2023-10-25 23:45:56 +0300

    add JS functionality

--- sidebar/script.js
+++ sidebar/script.js
@@ -1,6 +1,14 @@
-const navToggle = document.querySelector(".nav-toggle");
-const links = document.querySelector(".links");
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
 
-navToggle.addEventListener("click", function () {
-  links.classList.toggle("show-links");
+console.log("intra", sidebar);
+toggleBtn.addEventListener("click", function () {
+  sidebar.classList.toggle("show-sidebar");
+});
+
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.toggle("show-sidebar");
+  //or
+  sidebar.classList.remove("show-siderbar");
 });
