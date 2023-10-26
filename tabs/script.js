9d8ac61b8de9d90551513bd08d3897f28015fff4
sergiu_derecichei <d93sergiu@gmail.com>
2023-10-26 22:34:59 +0300

    Add tabs functionality

---
+++ tabs/script.js
@@ -0,0 +1,18 @@
+const btns = document.querySelectorAll(".tab-btn");
+const content = document.querySelectorAll(".content");
+const about = document.querySelector(".about");
+
+about.addEventListener('click', function(e) {
+  const id = e.target.dataset.id;
+  if(id) {
+    btns.forEach(btn => {
+      btn.classList.remove("active");
+      e.target.classList.add("active");
+    });
+    content.forEach((item) =>{
+      item.classList.remove("active");
+    })
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+})
