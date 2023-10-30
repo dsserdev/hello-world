cfb42374dc556d932ce6cbf85bada7a1444b086d
sergiu_derecichei <d93sergiu@gmail.com>
2023-10-31 00:42:36 +0200

    finish countdown project

--- countdown/script.js
+++ countdown/script.js
@@ -24,9 +24,15 @@ const weekdays = [
 
 const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
-const items = document.querySelector(".deadline-format h4");
+const items = document.querySelectorAll(".deadline-format h4");
 
-let futureDate = new Date(2023, 9, 30, 11, 59, 0);
+// let futureDate = new Date(2023, 9, 31, 23, 59, 0);
+let tempDate = new Date();
+let tempYear= tempDate.getFullYear();
+let tempMonth= tempDate.getMonth();
+let tempDay= tempDate.getDate();
+
+let futureDate = new Date(tempYear, tempMonth, tempDay + 10, 23, 59, 0);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
@@ -39,3 +45,43 @@ const date = futureDate.getDate();
 const weekday = weekdays[futureDate.getDay()];
 
 giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
+
+//future time
+const futureTime = futureDate.getTime();
+function getRemainingTime() {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+
+  let days = t / oneDay;
+  days = Math.floor(days);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
+
+  const values = [days, hours, minutes, seconds];
+
+  function format(item) {
+    if (item < 1) {
+      return (item = `0${item}`);
+    }
+    return item;
+  }
+
+  items.forEach(function (item, index) {
+    item.innerHTML = format(values[index]);
+  });
+  if (t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML  = `<h4 class="expired">Sorry, this giveaway has expired!</h4>`
+  }
+}
+
+let countdown = setInterval(getRemainingTime, 1000);
+getRemainingTime();
+
+
+//add 10 days always
