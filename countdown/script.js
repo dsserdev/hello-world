e3d1662405f74b3cb573165553d9669e7bc5c969
sergiu_derecichei <d93sergiu@gmail.com>
2023-10-30 23:59:24 +0200

    Progress countdown

---
+++ countdown/script.js
@@ -0,0 +1,40 @@
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const itens = document.querySelector(".deadline-format h4");
+
+let futureDate = new Date(2023, 10, 30, 11, 59, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+const month = futureDate.getMonth();
+month = months[month];
+const date = futureDate.getDay();
+const weekday = futureDate.getDay();
+
+giveaway.textContent = `giveaway ends on ${date} ${month} ${year} ${hours}:${minutes}am`;
